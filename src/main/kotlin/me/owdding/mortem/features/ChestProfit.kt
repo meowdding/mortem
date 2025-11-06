5657f560e38412467d5273381770d2d462621282
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-06 16:13:19 +0100

    idk this might work
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/mortem/features/ChestProfit.kt
+++ src/main/kotlin/me/owdding/mortem/features/ChestProfit.kt
@@ -3,15 +3,38 @@ package me.owdding.mortem.features
 import me.owdding.ktmodules.Module
 import me.owdding.lib.builder.LayoutFactory
 import me.owdding.mortem.utils.InventorySideGui
+import me.owdding.mortem.utils.colors.MortemColors
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
+import tech.thatgravyboat.skyblockapi.api.item.calculator.getItemValue
+import tech.thatgravyboat.skyblockapi.utils.extentions.getSkyBlockId
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
+import tech.thatgravyboat.skyblockapi.utils.text.TextBuilder.append
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 
 @Module
 object ChestProfit : InventorySideGui(".* Chest") {
-    override val enabled: Boolean
-        get() = true
+    override val enabled: Boolean get() = true
 
     override fun ContainerInitializedEvent.getLayout() = LayoutFactory.vertical {
-        string("meow")
+        val items = containerItems.filterNot { it.getSkyBlockId() == null }.associateWith { it.getItemValue() }
+
+        string("TITLE")
+        string("")
+        items.forEach { (k, v) ->
+            singularComponent {
+                component(k.hoverName)
+                string(": ") { color = TextColor.GRAY }
+                string(v.price.toFormattedString()) { color = MortemColors.BETTER_GOLD }
+            }
+        }
+        string("")
+        string("Total: ") {
+            color = TextColor.GRAY
+            append(items.values.sumOf { it.price }.toFormattedString()) { color = MortemColors.BETTER_GOLD }
+        }
+
+        // TODO: chestprice, keyprice
     }
 
 }
--- src/main/kotlin/me/owdding/mortem/utils/colors/MortemColors.kt
+++ src/main/kotlin/me/owdding/mortem/utils/colors/MortemColors.kt
@@ -4,4 +4,5 @@ object MortemColors {
     const val BASE_TEXT = 0xcdd6f4
     const val SEPARATOR = 0x585b70
     const val HIGHLIGHT = 0xcba6f7
+    const val BETTER_GOLD = 0xfc6f03
 }
