020665fe572d5fa3ed44003a945b021a20bd074f
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-17 23:12:55 +0100

    feat: regex parsing from tablist

--- src/main/kotlin/me/owdding/mortem/features/ScoreCalculator.kt
+++ src/main/kotlin/me/owdding/mortem/features/ScoreCalculator.kt
@@ -2,6 +2,19 @@ package me.owdding.mortem.features
 
 import tech.thatgravyboat.skyblockapi.api.area.dungeon.DungeonFloor
 import tech.thatgravyboat.skyblockapi.api.data.Perk
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyIn
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyWidget
+import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
+import tech.thatgravyboat.skyblockapi.api.events.hypixel.ServerChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidget
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidgetChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.location.ServerDisconnectEvent
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFloatValue
+import tech.thatgravyboat.skyblockapi.utils.extentions.toIntValue
+import tech.thatgravyboat.skyblockapi.utils.regex.RegexUtils.anyMatch
+import tech.thatgravyboat.skyblockapi.utils.regex.matchWhen
 import kotlin.math.floor
 import kotlin.math.roundToInt
 import kotlin.time.Duration
@@ -17,19 +30,35 @@ Each death 2, except first death with spirit pet
 // --- EXPLORATION --- max 100
 The Exploration Score depends on the percentage of dungeon completed, along with the percentage of secrets achieved. It is calculated as such:
 Explore=⌊60*(CompletedRooms/TotalRooms)⌋+⌊(40*SecretPercentFound/SecretPercentNeeded)⌋
-Where the percentage of needed secrets is:
-30%,On Floor 1
-40%,On Floor 2
-50%,On Floor 3
-60%,On Floor 4
-70%,On Floor 5
-85%,On Floor 6
-100%,On Floor 7 and Master Mode
+
+
+Discoveries: 4
+ Secrets Found: 4
+ Crypts: 0
+
+Dungeon: Catacombs
+ Opened Rooms: 34
+ Completed Rooms: 16
+ Secrets Found: 7.8%
+ Time: 06m 39s
+
+Puzzles: (3)
+ Quiz: [✖] ()
+ Three Weirdos: [✔]
+ ???: [✦]
 
  */
 
 object ScoreCalculator {
-    val requirements = mapOf(
+    // <editor-fold desc="Regex Wall">
+    // --Chat--
+    private val mimicKilledRegex = ".*(\$SKYTILS-DUNGEON-SCORE-MIMIC\$|Mimic [Dd]ead!|Mimic Killed!)$".toRegex()
+    // -- Tab--
+    private val cryptsRegex = " Crypts: (?<amount>\\d+)".toRegex()
+    private val secretPercentageRegex = " Secrets Found: (?<percentage>[\\d.]+)%".toRegex()
+    // </editor-fold>
+
+    private val requirements = mapOf(
         DungeonFloor.E to Requirements(0.3,600), // TODO
         DungeonFloor.F1 to Requirements(0.3,600),
         DungeonFloor.F2 to Requirements(0.4,600),
@@ -56,12 +85,13 @@ object ScoreCalculator {
 
     private var mimicKilled = false
     private var cryptsKilled = 0
+    private var secretPercentage = 0f
 
     fun getScore(time: Duration, floor: DungeonFloor): Int {
         val req = requirements[floor] ?: return 0
         return listOf(
             getSkillScore(),
-            getExplorationScore(),
+            getExplorationScore(req),
             getSpeedScore(time, req),
             getBonusScore(),
         ).sum()
@@ -71,8 +101,10 @@ object ScoreCalculator {
         return 0
     }
 
-    private fun getExplorationScore(): Int {
-        return 0
+    private fun getExplorationScore(req: Requirements): Int {
+        val roomsScore = 0
+        val secretsScore = floor((40 * secretPercentage / req.secretPercentNeeded)).roundToInt()
+        return roomsScore + secretsScore
     }
 
     private fun getSpeedScore(time: Duration, req: Requirements): Int {
@@ -88,6 +120,7 @@ object ScoreCalculator {
     }
 
     private fun getBonusScore(): Int = buildList {
+        // TODO: prince maybe
         if (Perk.EZPZ.active) add(10)
         if (mimicKilled) add(2)
         add(cryptsKilled.coerceAtMost(5))
@@ -98,5 +131,43 @@ object ScoreCalculator {
         val speedTime: Duration,
     ) {
         constructor(secretPercentNeeded: Double, speedTime: Int) : this(secretPercentNeeded, speedTime.seconds)
+    }
+
+    @Subscription(ServerChangeEvent::class, ServerDisconnectEvent::class)
+    fun reset() {
+        mimicKilled = false
+        cryptsKilled = 0
+    }
+
+    @Subscription
+    @OnlyIn(SkyBlockIsland.THE_CATACOMBS)
+    fun onChat(event: ChatReceivedEvent.Pre) {
+        matchWhen(event.text) {
+            case(mimicKilledRegex) {
+                mimicKilled = true
+            }
+        }
+    }
+
+    @Subscription
+    @OnlyWidget(TabWidget.DISCOVERIES)
+    fun onDiscoveriesWidget(event: TabWidgetChangeEvent) {
+        cryptsRegex.anyMatch(event.new, "amount") { (amount) ->
+            cryptsKilled = amount.toIntValue()
+        }
+    }
+
+    @Subscription
+    @OnlyWidget(TabWidget.AREA)
+    fun onAreaWidget(event: TabWidgetChangeEvent) {
+        secretPercentageRegex.anyMatch(event.new, "percentage") { (percentage) ->
+            this.secretPercentage == percentage.toFloatValue() / 100f
+        }
+    }
+
+    @Subscription
+    @OnlyWidget(TabWidget.PUZZLES)
+    fun onPuzzlesWidget(event: TabWidgetChangeEvent) {
+
     }
 }
