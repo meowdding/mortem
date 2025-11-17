49b9f58f24902928b7f023e507550aa15c3dd681
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-17 20:14:17 +0100

    feat: init score calc
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

---
+++ src/main/kotlin/me/owdding/mortem/features/ScoreCalculator.kt
@@ -0,0 +1,102 @@
+package me.owdding.mortem.features
+
+import tech.thatgravyboat.skyblockapi.api.area.dungeon.DungeonFloor
+import tech.thatgravyboat.skyblockapi.api.data.Perk
+import kotlin.math.floor
+import kotlin.math.roundToInt
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+/*
+
+// --- SKILL --- max 100
+The Skill score ranges from 20 to 100, with 20 being the lowest score achievable. The Skill Score is calculated as such:
+Skill=20+⌊80*(CompletedRoomsTotalRooms)⌋−(10*FailedPuzzles)−DeathPenalty
+Each death 2, except first death with spirit pet
+
+// --- EXPLORATION --- max 100
+The Exploration Score depends on the percentage of dungeon completed, along with the percentage of secrets achieved. It is calculated as such:
+Explore=⌊60*(CompletedRooms/TotalRooms)⌋+⌊(40*SecretPercentFound/SecretPercentNeeded)⌋
+Where the percentage of needed secrets is:
+30%,On Floor 1
+40%,On Floor 2
+50%,On Floor 3
+60%,On Floor 4
+70%,On Floor 5
+85%,On Floor 6
+100%,On Floor 7 and Master Mode
+
+ */
+
+object ScoreCalculator {
+    val requirements = mapOf(
+        DungeonFloor.E to Requirements(0.3,600), // TODO
+        DungeonFloor.F1 to Requirements(0.3,600),
+        DungeonFloor.F2 to Requirements(0.4,600),
+        DungeonFloor.F3 to Requirements(0.5,600),
+        DungeonFloor.F4 to Requirements(0.6,720),
+        DungeonFloor.F5 to Requirements(0.7,600),
+        DungeonFloor.F6 to Requirements(0.85,720),
+        DungeonFloor.F7 to Requirements(1.0,840),
+        DungeonFloor.M1 to Requirements(1.0, 480),
+        DungeonFloor.M2 to Requirements(1.0, 480),
+        DungeonFloor.M3 to Requirements(1.0, 480),
+        DungeonFloor.M4 to Requirements(1.0, 480),
+        DungeonFloor.M5 to Requirements(1.0, 480),
+        DungeonFloor.M6 to Requirements(1.0, 600),
+        DungeonFloor.M7 to Requirements(1.0, 840),
+    )
+    private val speedDecreasePercentage = mapOf(
+        0.0..0.2 to 0.02,
+        0.2..0.4 to 0.04,
+        0.4..0.5 to 0.05,
+        0.5..0.6 to 0.06,
+        0.6..Double.MAX_VALUE to 0.07,
+    )
+
+    private var mimicKilled = false
+    private var cryptsKilled = 0
+
+    fun getScore(time: Duration, floor: DungeonFloor): Int {
+        val req = requirements[floor] ?: return 0
+        return listOf(
+            getSkillScore(),
+            getExplorationScore(),
+            getSpeedScore(time, req),
+            getBonusScore(),
+        ).sum()
+    }
+
+    private fun getSkillScore(): Int {
+        return 0
+    }
+
+    private fun getExplorationScore(): Int {
+        return 0
+    }
+
+    private fun getSpeedScore(time: Duration, req: Requirements): Int {
+        if (time <= req.speedTime) return 100
+
+        val percentOver = (time / req.speedTime) - 1
+        val lost = speedDecreasePercentage.mapNotNull { (range, step) ->
+            if (percentOver < range.start) return@mapNotNull null
+            val delta = (percentOver - range.start).coerceAtMost(range.endInclusive - range.start)
+            delta / step
+        }
+        return 100 - floor(lost.sum()).roundToInt()
+    }
+
+    private fun getBonusScore(): Int = buildList {
+        if (Perk.EZPZ.active) add(10)
+        if (mimicKilled) add(2)
+        add(cryptsKilled.coerceAtMost(5))
+    }.sum()
+
+    data class Requirements(
+        val secretPercentNeeded: Double,
+        val speedTime: Duration,
+    ) {
+        constructor(secretPercentNeeded: Double, speedTime: Int) : this(secretPercentNeeded, speedTime.seconds)
+    }
+}
