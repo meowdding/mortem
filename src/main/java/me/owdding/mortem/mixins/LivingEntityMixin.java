ba82f73644862e590a954f332dc97ace55771a22
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-20 23:11:44 +0100

    fix event posting

--- src/main/java/me/owdding/mortem/mixins/LivingEntityMixin.java
+++ src/main/java/me/owdding/mortem/mixins/LivingEntityMixin.java
@@ -6,13 +6,14 @@ import me.owdding.mortem.events.EntityDeathEvent;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 
     @WrapMethod(method = "die")
     private void onDieWrap(DamageSource damageSource, Operation<Void> original) {
-        new EntityDeathEvent((LivingEntity) (Object) this, damageSource);
+        new EntityDeathEvent((LivingEntity) (Object) this, damageSource).post(SkyBlockAPI.getEventBus());
         original.call(damageSource);
     }
 
